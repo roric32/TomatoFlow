fd1852c1834d8cccc0a467426f9a5fb4879282ad
Brandon Thompson <brandonkthompson@gmail.com>
2020-03-26 00:03:28 -0400

    Added assemblydescription.

--- TomatoFlow/Properties/AssemblyInfo.cs
+++ TomatoFlow/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TomatoFlow")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple timeboxing app.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TomatoFlow")]
